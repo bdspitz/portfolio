e4f9cd13883fb8c5e6cacf2773d5507aa2ea9903
Brad Spitz <bdspitz@gmail.com>
2016-09-28 18:35:26 -0700

    Add portfolio page

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Portfolio
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
   end
 end
